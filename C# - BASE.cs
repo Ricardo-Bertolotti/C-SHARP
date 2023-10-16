5ae8c086e2874c3adb5262754e334c26c2f85202
Ricardo Bertolotti <59855897+Ricardo-Bertolotti@users.noreply.github.com>
2023-10-16 18:19:30 -0300

    Add files via upload

---
+++ C# - BASE.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# BASE");
+        }
+    }	
+}
